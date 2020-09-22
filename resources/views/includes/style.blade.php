aa6599c4c07d269c711d32ed0563f894b7cd76ee
muhsinahadi <muhsinahadi25@gmail.com>
2020-09-22 17:04:13 +0700

    initial commit

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,9 @@
+    <!-- Bootstrap core CSS     -->
+    <link href="{{ url('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="{{ url('assets/css/material-dashboard.css') }}" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    {{-- <link href="../assets/css/demo.css" rel="stylesheet" /> --}}
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
